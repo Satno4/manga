753e320992cff04160478ac4734ba34953b4b488
satno4 <besariongagelidze@gmail.com>
2021-07-11 02:48:52 +0400

    theming thing

---
+++ web/themes/custom/anime/js/main.js
@@ -0,0 +1,11 @@
+// alert('works');
+// let dumbBitch = document.getElementsByClassName('messages__wrapper');
+// dumbBitch.remove();
+setTimeout(function() {
+    let dumbBitch = document.getElementsByClassName('messages--error');
+    for (var i = 0; i<dumbBitch.length; i++){
+        dumbBitch[i].remove();
+    }
+    // alert("done");
+}, 1000);
+
